e18c35d6d0d475e643068bb0c9f58405e725a218
PrimalCatReal <romanenko.denis2003@gmail.com>
2024-02-12 16:09:33 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+'use client'
 import "@/styles/globals.css";
 import { Metadata ,Viewport } from "next";
 import { siteConfig } from "@/config/site";
@@ -7,6 +8,14 @@ import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
+import {
+	RecoilRoot,
+	atom,
+	selector,
+	useRecoilState,
+	useRecoilValue,
+  } from 'recoil';
+
 export const viewport: Viewport = {
 	themeColor: [
 		{ media: "(prefers-color-scheme: light)", color: "white" },
@@ -14,19 +23,29 @@ export const viewport: Viewport = {
 	],
 }
 
-export const metadata: Metadata = {
-	title: {
-		default: siteConfig.name,
-		template: `%s - ${siteConfig.name}`,
-	},
-	description: siteConfig.description,
+// or Dynamic metadata
+// export async function generateMetadata() {
+// 	return {
+// 		title: {
+// 			default: siteConfig.name,
+// 			template: `%s - ${siteConfig.name}`,
+// 		},
+// 	}
+// }
+
+// export const metadata: Metadata = {
+// 	title: {
+// 		default: siteConfig.name,
+// 		template: `%s - ${siteConfig.name}`,
+// 	},
+// 	description: siteConfig.description,
 	
-	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
-	},
-};
+// 	icons: {
+// 		icon: "/favicon.ico",
+// 		shortcut: "/favicon-16x16.png",
+// 		apple: "/apple-touch-icon.png",
+// 	},
+// };
 
 export default function RootLayout({
 	children,
@@ -34,34 +53,36 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<head />
-			<body
-				className={clsx(
-					"min-h-screen  font-sans antialiased  bg-background",
-					fontSans.variable
-				)}
-			>
-				<Providers themeProps={{ attribute: "data-theme", defaultTheme: "system" }}>
-					<div className="relative flex flex-col ">
-						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow ">
-							{children}
-						</main>
-						<footer className="w-full flex items-center justify-center py-3">
-							<Link
-								isExternal
-								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-								title="nextui.org homepage"
-							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI + ShadCN + DaisyUI</p>
-							</Link>
-						</footer>
-					</div>
-				</Providers>
-			</body>
-		</html>
+		<RecoilRoot>
+			<html lang="en" suppressHydrationWarning>
+				<head />
+				<body
+					className={clsx(
+						"min-h-screen  font-sans antialiased  bg-background",
+						fontSans.variable
+					)}
+				>
+					<Providers themeProps={{ attribute: "data-theme", defaultTheme: "system" }}>
+						<div className="relative flex flex-col ">
+							<Navbar />
+							<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow ">
+								{children}
+							</main>
+							<footer className="w-full flex items-center justify-center py-3">
+								<Link
+									isExternal
+									className="flex items-center gap-1 text-current"
+									href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+									title="nextui.org homepage"
+								>
+									<span className="text-default-600">Powered by</span>
+									<p className="text-primary">NextUI + ShadCN + DaisyUI</p>
+								</Link>
+							</footer>
+						</div>
+					</Providers>
+				</body>
+			</html>
+		</RecoilRoot>
 	);
 }
